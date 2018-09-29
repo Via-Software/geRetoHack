3de4e1a768f3b3068343cd4103ac1ce1ff0150b4
Vladimir Juárez <ivladimirjuarez@outlook.com>
2018-09-29 18:04:55 -0500

    fixin' rutas method get

--- src/main.js
+++ src/main.js
@@ -6,7 +6,11 @@ import store from './store';
 import './registerServiceWorker';
 import './assets/css/bootstrap.min.css';
 Vue.config.productionTip = false;
+import axios from 'axios';
 
+axios.defaults.baseURL = 'http://192.168.0.146:8100';
+// axios.defaults.headers.common['Authorization'] = 'fasfdsa'
+axios.defaults.headers.get['Accepts'] = 'application/json';
 new Vue({
   router,
   store,
--- src/views/Rutas.vue
+++ src/views/Rutas.vue
@@ -1,12 +1,30 @@
 <template>
     <div>
-        
+        <button type="button" class="btn btn-primary btn-lg btn-block" @click.prevent="check">Checar rutas</button>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name:'Rutas',
-
+    methods:{
+        check(){
+            // https://jsonplaceholder.typicode.com/todos
+            // http://192.168.0.146:8080/buses
+            axios.get('/buses')
+            .then(function (response) {  
+                console.log(response);
+            })
+            .catch(function (error) {  
+                console.log(error);
+                
+            });
+        },
+    },
+    mounted(){
+        
+    }
 }
 </script>
