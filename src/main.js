eaf2d50a2114a922d0211bb318b5e7c1c451fbee
Vladimir Juárez <ivladimirjuarez@outlook.com>
2018-09-29 19:03:04 -0500

    addin vue bootstrap

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 /* jshint esversion:6 */
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 import './assets/css/bootstrap.min.css';
-
 Vue.config.productionTip = false;
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 axios.defaults.baseURL = 'http://192.168.0.146:8100';
 // axios.defaults.headers.common['Authorization'] = 'fasfdsa'
 axios.defaults.headers.get['Accepts'] = 'application/json';
+
+Vue.use(BootstrapVue);
 new Vue({
   router,
   store,
