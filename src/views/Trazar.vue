a1a8f7628240d3a13ff2a00914552260ccd4243e
Vladimir Juárez <ivladimirjuarez@outlook.com>
2018-09-29 22:56:29 -0500

    .

--- src/views/Trazar.vue
+++ src/views/Trazar.vue
@@ -1,20 +1,5 @@
 <template>
     <div>
-        <GmapMap
-        :center="{lat:20.658283, lng:-100.349658}"
-        :zoom="14"
-        map-type-id="terrain"
-        style="width: 500px; height: 300px"
-        >
-        <GmapMarker
-            :key="index"
-            v-for="(m, index) in markers"
-            :position="m.position"
-            :clickable="true"
-            :draggable="true"
-            @click="center=m.position"
-        />
-        </GmapMap>
     </div>
 </template>
 
