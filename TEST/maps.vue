d2e06847f41dabd44d4809473e0c9ae7e3b01c31
Antonio2396 <43594671+Antonio2396@users.noreply.github.com>
2018-09-29 19:13:14 -0500

    test map

---
+++ TEST/maps.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <h4>Soy el Mapakdnjdjmndjnfdjn</h4>
+    <div class="container" id="bodyHome"> 
+      <!-- <button type="button" class="btn btn-primary btn-lg btn-block" @click="check">Rutas</button> -->
+      <router-link to="/rutas" class="btn btn-primary btn-lg btn-block" >Rutas</router-link>
+      <button type="button" class="btn btn-primary btn-lg btn-block">Trazar Ruta</button>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  components: {
+    // HelloWorld
+  },
+  methods:{
+    check(){
+
+    }
+  }
+}
+</script>
+
+<style>
+  #bodyHome{
+    position: absolute;
+    bottom: 10%;
+  }
+</style>
