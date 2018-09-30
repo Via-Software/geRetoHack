c3070fd40a8ac1fe5870055f006866e30bc7861e
Vladimir Juárez <ivladimirjuarez@outlook.com>
2018-09-29 22:55:14 -0500

    reset maps

--- src/views/Trazar.vue
+++ src/views/Trazar.vue
@@ -1,40 +1,18 @@
 <template>
     <div>
-        <GmapMap
-        :center="{lat:20.658283, lng:-100.349658}"
-        :zoom="14"
-        map-type-id="terrain"
-        style="width: 100%; height: 500px"
-        >
-        <!-- <GmapMarker
-            :key="index"
-            v-for="(m, index) in markers"
-            :position="m.position"
-            :clickable="true"
-            :draggable="true"
-            @click="center=m.position"
-        /> -->
-        <google-marker :position="position" :clickable="true"></google-marker>
-        </GmapMap>
+
     </div>
 </template>
 
 <script>
+const googleMapsClient = require('@google/maps').createClient({
+  key: 'AIzaSyBs4Icy6WhjZ73-pqjPLKsq0p8I06AsCII'
+});
+
 export default {
     name:'Trazar',
     data(){
         return{
-            center: {
-                lat:20.658283, 
-                lng:-100.349658
-            },
-            markers:[{
-                position: {
-                    lat:20.658283, 
-                    lng:-100.349658
-                }
-            }
-            ]
         }
     }
 }
